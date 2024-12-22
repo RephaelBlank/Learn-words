b785c957f13b9decb1e52ab03ddb6d5264d16ef5
Rephael <rephaelblank@gmail.com>
2024-12-22 13:50:39 +0200

    add execution task

--- src/performance/performance.service.ts
+++ src/performance/performance.service.ts
@@ -128,31 +128,65 @@ export class PerformanceService {
             throw new NotFoundException('Task execution not found');
         }
 
-        const words = taskExecution.assignedTask.tasks.words.map(word => ({
-              id: word.wordID,
-              content: word.wordName,
-            }));
-        
-        const definitions =  taskExecution.assignedTask.tasks.words.map(word => ({
-              id: word.definition.definitionID,
-              content: word.definition.definition,
-            }));
-        
-        const shuffledWords = words.sort(() => Math.random() - 0.5);
-        const shuffledDefinitions = definitions.sort(() => Math.random() - 0.5);
-        
-          return {
-            words: shuffledWords,
-            definitions: shuffledDefinitions,
-          };
-
+        return taskExecution; 
         }
 
-    async executionTask (executionID: number, answers: JSON){
+    async getTaskToExecution (executionID: number){
+      const taskExecution = await this.findTaskExecutionById(executionID);
+
+      const words = taskExecution.assignedTask.tasks.words.map(word => ({
+        id: word.wordID,
+        content: word.wordName,
+      }));
+  
+      const definitions =  taskExecution.assignedTask.tasks.words.map(word => ({
+        id: word.definition.definitionID,
+        content: word.definition.definition,
+      }));
+
+      const shuffledWords = words.sort(() => Math.random() - 0.5);
+      const shuffledDefinitions = definitions.sort(() => Math.random() - 0.5);
+      
+        return {
+          words: shuffledWords,
+          definitions: shuffledDefinitions,
+        };
+    }
+
+    async verifyWordDefinitionPairs(executionID, pairs: { wordID: number; definitionID: number }[]) {
+
+        const wordIDs = pairs.map(pair => pair.wordID);
+
+        const taskExecution = await this.findTaskExecutionById(executionID);
+        const words = taskExecution.assignedTask.tasks.words;
+
+        const validationMap = words.reduce((map, word) => {
+            map[word.wordID] = word.definition.definitionID;
+            return map;
+            }, {} as Record<number, number>);
+          
+        const results = pairs.map(pair => {
+            const isValid = validationMap[pair.wordID] === pair.definitionID;
+            return {
+                ...pair,
+                isValid,
+              };
+            });
+          
+        return results;
+    }
+
+    async executionTask (executionID: number, answers){
         const taskExecution = await this.tasksExecutionsModel.findOne({
             where: { executionID }}); 
-        taskExecution.answers = answers; 
+        //taskExecution.answers = answers; 
         taskExecution.status = 'COMPLETED';
+
+        //const pairs = JSON.parse(answers); 
+        const result = await this.verifyWordDefinitionPairs (executionID,answers); 
+
+        console.log(result); 
+
         taskExecution.score = 60; 
 
         await taskExecution.save(); 
