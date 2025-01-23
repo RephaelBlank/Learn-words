6c163f841f9349acfb41b59d8dafa785c715509c
Rephael <rephaelblank@gmail.com>
2025-01-23 11:23:31 +0200

    get assigned id of assigned tasks with classes details

--- src/classes/classes.service.ts
+++ src/classes/classes.service.ts
@@ -31,7 +31,7 @@ export class ClasseService {
         },
         {
           model: AssignedTasks, 
-          attributes: ['taskID'],
+          attributes: ['taskID','assignedID'],
           include: [Tasks]
         },
       ],
